e74a91ec46e97cdc4becd16522b093f5613fa0cb
Mathias Buus <mathiasbuus@gmail.com>
2016-11-22 23:22:36 -0300

    expose add encoders

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
-var ascii = createString('ascii')
-var utf8 = createString('utf-8')
-var hex = createString('hex')
-var base64 = createString('base64')
-var ucs2 = createString('ucs2')
-var utf16le = createString('utf16le')
+module.exports = codecs
 
-var json = {
-  encode: function (obj) {
-    return new Buffer(JSON.stringify(obj))
-  },
-  decode: function (buf) {
-    return JSON.parse(buf.toString())
-  }
-}
-
-var binary = {
-  encode: function (obj) {
+codecs.ascii = createString('ascii')
+codecs.utf8 = createString('utf-8')
+codecs.hex = createString('hex')
+codecs.base64 = createString('base64')
+codecs.ucs2 = createString('ucs2')
+codecs.utf16le = createString('utf16le')
+codecs.ndjson = createJSON(true)
+codecs.json = createJSON(false)
+codecs.binary = {
+  encode: function encodeBinary (obj) {
     return typeof obj === 'string' ? new Buffer(obj, 'utf-8') : obj
   },
-  decode: function (buf) {
+  decode: function decodeBinary (buf) {
     return buf
   }
 }
 
-module.exports = function (fmt) {
+function codecs (fmt) {
   if (typeof fmt === 'object' && fmt && fmt.encode && fmt.decode) return fmt
 
   switch (fmt) {
-    case 'json': return json
-
-    case 'ascii': return ascii
-
+    case 'ndjson': return codecs.ndjson
+    case 'json': return codecs.json
+    case 'ascii': return codecs.ascii
     case 'utf-8':
-    case 'utf8': return utf8
-
-    case 'hex': return hex
-
-    case 'base64': return base64
-
+    case 'utf8': return codecs.utf8
+    case 'hex': return codecs.hex
+    case 'base64': return codecs.base64
     case 'ucs-2':
-    case 'ucs2': return ucs2
-
+    case 'ucs2': return codecs.ucs2
     case 'utf16-le':
-    case 'utf16le': return utf16le
+    case 'utf16le': return codecs.utf16le
   }
 
-  return binary
+  return codecs.binary
+}
+
+function createJSON (newline) {
+  return {
+    encode: newline ? encodeNDJSON : encodeJSON,
+    decode: function decodeJSON (buf) {
+      return JSON.parse(buf.toString())
+    }
+  }
+
+  function encodeJSON (val) {
+    return new Buffer(JSON.stringify(val))
+  }
+
+  function encodeNDJSON (val) {
+    return new Buffer(JSON.stringify(val) + '\n')
+  }
 }
 
 function createString (type) {
   return {
-    encode: function (obj) {
-      return new Buffer(obj, type)
+    encode: function encodeString (val) {
+      if (typeof val !== 'string') val = val.toString()
+      return new Buffer(val, type)
     },
-    decode: function (buf) {
+    decode: function decodeString (buf) {
       return buf.toString(type)
     }
   }
